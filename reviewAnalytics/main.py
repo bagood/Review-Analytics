15cf4087064acaf4393799e2175bbed22018db28
Ariabagus Wisnuwardhana Mustikoatmojo <4riabagus@gmail.com>
2025-08-30 14:52:57 +0700

    updates the ipynb files

--- reviewAnalytics/main.py
+++ reviewAnalytics/main.py
@@ -26,13 +26,14 @@ def categorize_aspects_of_food_review(data):
     data.dropna(subset='Food - summary', inplace=True)
     all_menu_reviewed = pd.DataFrame()
 
-    for summary, restaurant in zip(data['Food - summary'].values, data['restaurant_name'].values):
+    for summary, restaurant, restaurant_location in zip(data['Food - summary'].values, data['review_source'].values):
         response = _categorize_aspects_of_food_review(summary)
 
         for menu, feedbacks in response.items():
             menu_reviewed = pd.DataFrame(feedbacks)
             menu_reviewed['menu'] = menu
             menu_reviewed['restaurant'] = restaurant
+            menu_reviewed['restaurant_location'] = restaurant_location
 
             all_menu_reviewed = pd.concat((all_menu_reviewed, menu_reviewed))
 
